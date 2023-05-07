86fe1ce1093fa7b531da4dd1a63ddd5a8fea2795
Pellyyy <zabookinkajou@protonmail.com>
2023-05-07 16:30:34 +0200

    implementation of post form wip

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,8 @@ function App() {
                         avatarId={post.avatarId}
                     />
                 ))}
-                <Form />
             </div>
+            <Form />
         </div>
     )
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,53 @@
 export default function Form() {
     return (
-        <div className="form-control max-w-md">
-            <label htmlFor=""></label>
-            <input className="input input-bordered" type="text" />
-        </div>
+        <form
+            onSubmit={(e) => {
+                e.preventDefault()
+                alert("form sent!")
+            }}
+        >
+            <div className="form-control max-w-lg mx-auto pt-12">
+                <label htmlFor="username" className="label">
+                    Username
+                </label>
+                <input
+                    id="username"
+                    placeholder="Your name..."
+                    className="input input-bordered focus:outline-primary"
+                    type="text"
+                />
+                <label htmlFor="title" className="label pt-4">
+                    Title
+                </label>
+                <input
+                    id="title"
+                    placeholder="Title for your post..."
+                    className="input input-bordered focus:outline-primary"
+                    type="text"
+                />
+                <label htmlFor="title" className="label pt-4">
+                    Text
+                </label>
+                <textarea
+                    id="title"
+                    placeholder="Don't be shy, tell me what you think..."
+                    className="textarea textarea-bordered h-36 focus:outline-primary"
+                />
+                <div className="flex w-full justify-between">
+                    <button
+                        type="submit"
+                        className="btn btn-primary w-1/3 mt-4 "
+                    >
+                        submit
+                    </button>
+                    <button
+                        type="reset"
+                        className="btn btn-outline btn-secondary w-1/3 mt-4 "
+                    >
+                        reset form
+                    </button>
+                </div>
+            </div>
+        </form>
     )
 }
